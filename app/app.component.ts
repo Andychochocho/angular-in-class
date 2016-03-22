2ad419397eb01775b452503db89f2bdd3505b96a
andrew cho and timothy durward <timothy.a.durward@gmail.com>
2016-03-22 11:56:44 -0700

    add TaskComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component, EventEmitter } from 'angular2/core';
 
 @Component({
+  selector: 'task-display',
+  inputs: ['task'],
+template:`
+  <h3> {{ task.description }} </h3>
+`
+})
+
+export class TaskComponent {
+ public task: Task;
+}
+
+@Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
+  directives: [TaskComponent],
   template: `
-  <h3 *ngFor="#currentTask of taskList"
+  <task-display *ngFor="#currentTask of taskList"
   (click)="taskClicked(currentTask)"
-  [class.selected]="currentTask === selectedTask">
-  {{ currentTask.description }}
+  [class.selected]="currentTask === selectedTask"
+  [task]="currentTask">
+  </task-display>
   `
 })
 
